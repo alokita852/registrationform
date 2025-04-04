ed72a442350d9853f927ba502dae68c18e7bb233
alokita852 <rawatalokita@gmail.com>
2025-04-04 21:50:54 +0530

    2/04/25

---
+++ registraion.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$age = $_POST['age'];
+$course = $_POST['course'];
+echo  "Student Name: " .$name;
+echo "<br>";
+echo "<br>";
+echo  "Student Email: ".$email;
+echo "<br>";
+echo "<br>";
+echo "Student Age: ". $age;
+echo "<br>";
+echo "<br>";
+echo "Student Course: ". $course;
+
+
+
+?>
